ef6269a70a5fe636ea0dfddbf9b043cd3809d384
pig <576618121@qq.com>
2016-11-29 00:03:43 +0800

    request camera permission

---
+++ permissionslibrary/src/main/java/pig/dream/permissionslibrary/Constants.java
@@ -0,0 +1,12 @@
+package pig.dream.permissionslibrary;
+
+/**
+ * Created by pig on 16-11-28.
+ */
+
+public class Constants {
+
+    public final static String TAG = "PermissionsLibrary";
+
+    public final static int REQUEST_CAMERA = 0x100;
+}
---
+++ permissionslibrary/src/main/java/pig/dream/permissionslibrary/PermissionsUtils.java
@@ -0,0 +1,59 @@
+package pig.dream.permissionslibrary;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.view.View;
+
+/**
+ * 工具类 用于申请权限
+ *
+ * Created by pig on 16-11-28.
+ */
+
+public class PermissionsUtils {
+
+
+    public static void onRequestPermissionsResult() {
+
+    }
+
+    public static void checkSelfPermission() {
+//        ContextCompat.checkSelfPermission()
+    }
+
+    public static void requestCameraPermission(@NonNull final Activity activity, @NonNull final View view) {
+        Log.i(Constants.TAG, "CAMERA permission has NOT been granted. Requesting permission.");
+
+        // BEGIN_INCLUDE(camera_permission_request)
+        if (ActivityCompat.shouldShowRequestPermissionRationale(activity,
+                Manifest.permission.CAMERA)) {
+            // Provide an additional rationale to the user if the permission was not granted
+            // and the user would benefit from additional context for the use of the permission.
+            // For example if the user has previously denied the permission.
+            Log.i(Constants.TAG,
+                    "Displaying camera permission rationale to provide additional context.");
+            Snackbar.make(view, "请求照相机权限。",
+                    Snackbar.LENGTH_INDEFINITE)
+                    .setAction("确定", new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            ActivityCompat.requestPermissions(activity,
+                                    new String[]{Manifest.permission.CAMERA},
+                                    Constants.REQUEST_CAMERA);
+                        }
+                    })
+                    .show();
+        } else {
+            // Camera permission has not been granted yet. Request it directly.
+            ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.CAMERA},
+                    Constants.REQUEST_CAMERA);
+        }
+    }
+
+}
